0813c3c35756add5f2bc1549aaa1d0c140c92b81
naho1234 <naa01.1105ily@gmail.com>
2023-09-28 16:13:56 +0900

    [add]404ページ完了

--- 404.php
+++ 404.php
@@ -1,15 +1,25 @@
+<?php
+	/* Template Name:404ページ */
+?>
 <?php get_header(); ?>
+
 <main>
-    <section class="errorpage">
-        <div class="errorpage__inner">
-            <div class="errorpage__message">
-                <span class="errorpage__404message">404 Not Found</span>
-                <span class="errorpage__text">お探しのページは<br class="sp__show">見つかりませんでした。</span>
-            </div>
-            <div class="btn btn--subpage">
-                <a href="<?php echo esc_url( home_url( '/' ) ); ?>">TOPへ</a>
-            </div>
-        </div>
-    </section>
-  </main>
-<?php get_footer(); ?>
+	<?php get_template_part('template/breadcrumb');?>
+	<main>
+		<section class="errorpage">
+			<div class="errorpage__inner inner">
+				<h2 class="errorpage__message">
+					誠に申し訳ございませんが、<br>
+					お探しのページは一時的にアクセスができない状況にあるか、<br>
+					移動もしくは削除された可能性があります。<br>
+				</h2>
+				<div class="btn btn--subpage">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>">TOPページに戻る</a>
+				</div>
+			</div>
+		</section>
+	</main>
+
+	<?php get_template_part('template/contact-footer');?>
+
+	<?php get_footer() ;?>
--- page-thanks.php
+++ page-thanks.php
@@ -9,8 +9,10 @@ get_header() ;
 	<section class="thanks">
 		<div class="thanks__inner inner">
 			<h2 class="thanks__title">この度はお問い合わせをいただき誠にありがとうございます。</h2>
-			<p class="thanks__text">担当者より改めてご連絡させていただきますので、お待ちいただきますようよろしくお願いいたします。</p>
-			<a href="<?php echo home_url(''); ?>" class="thanks__top">トップページに戻る</a>
+			<p class="thanks__text">担当者より改めてご連絡させていただきますので、<br>お待ちいただきますようよろしくお願いいたします。</p>
+			<div class="thanks__top">
+				<a href="<?php echo home_url(''); ?>">トップページに戻る</a>
+			</div>
 		</div>
 	</section>
 
