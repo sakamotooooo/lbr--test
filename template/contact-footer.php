b2eda159e1153d9bb79395cf125561b750e40369
naho1234 <naa01.1105ily@gmail.com>
2023-09-29 07:04:54 +0900

    [add]LP共通

--- template/contact-footer.php
+++ template/contact-footer.php
@@ -13,17 +13,18 @@
 			ご相談・ご質問等ございましたら、<br class="u-mobile">
 			お気軽にお問い合わせください。
 		</p>
-		<div class="top-contact__items">
-			<div class="top-contact__item">
-				<div class="top-contact__telImg">
-					<img src="<?php echo get_template_directory_uri() ?>/images/phone.png" alt="電話アイコン">
+		<div class="top-contact__items consultation">
+			<div class="top-contact__item consultation__item">
+				<div class="consultation__telImg">
+					<img width="17" height="30" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/phone.png"
+						alt="電話アイコン">
 				</div>
-				<div class="top-contact__telDetails">
-					<p class="top-contact__telNumber">03-6276-5828</p>
-					<p class="top-contact__telTime">平日受付:9:00～18:00</p>
+				<div class="consultation__telDetails">
+					<p class="consultation__telNumber">03-6276-5828</p>
+					<p class="consultation__telTime">平日受付:9:00～18:00</p>
 				</div>
 			</div>
-			<div class="top-contact__item">
+			<div class="top-contact__item consultation__item">
 				<a href="<?php echo $contact; ?>">
 					<span>お問い合わせ</span>
 				</a>
