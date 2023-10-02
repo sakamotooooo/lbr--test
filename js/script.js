8881471b907decb3f1d18687816fb5f673234f56
naho1234 <naa01.1105ily@gmail.com>
2023-10-02 19:52:15 +0900

    [add]contact form 7 サンクスページへ

--- js/script.js
+++ js/script.js
@@ -37,6 +37,16 @@ jQuery(function ($) {
     // クリックしたタイトルのcontentを開閉
     $(this).next().slideToggle(300);
   });
+
+	  // ページ内スクロール
+		$('a[href^="#"]').click(function () {
+			const speed = 600;
+			let href = $(this).attr("href");
+			let target = $(href == "#" || href == "" ? "html" : href);
+			let position = target.offset().top;
+			$("body,html").animate({ scrollTop: position }, speed, "swing");
+			return false;
+		});
 });
 
 //topページスライダー
