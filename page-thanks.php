6b98713940483aa4ba2a0f93ce2da7ad872b5eb7
naho1234 <naa01.1105ily@gmail.com>
2023-10-10 15:22:24 +0900

    [add]footer完了

--- page-thanks.php
+++ page-thanks.php
@@ -25,26 +25,5 @@ get_header() ;
 			</div>
 		</div>
 	</section>
-
-	<!-- sectionのタイトル(blue)英語は小文字で入力してください（CSSで大文字にしてます） -->
-	<!-- <div class="section-heading">
-		<span class="section-heading__subtitle section-heading__subtitle--blue">feature</span>
-		<h2 class="section-heading__title section-heading__title--black">エルビーアールのこだわり</h2>
-	</div> -->
-
-	<!-- sectionのタイトル(white) -->
-	<!-- <div class="section-heading">
-		<span class="section-heading__subtitle section-heading__subtitle--white">feature</span>
-		<h2 class="section-heading__title section-heading__title--white">エルビーアールのこだわり</h2>
-	</div> -->
-
-	<!-- sectionのタイトル薄いのん英語は小文字で入力してください（CSSで大文字にしてます） -->
-	<!--<div>で囲ってそのdivで位置調整してください。<span></span>は共通です。
-	<div></div>はそれぞれにつけてください。 -->
-	<!-- <div class="top-contact__thin">⬅️位置調整のdiv(section-headingにposition:relativeつけてます)
-		<span class="section-heading__thin section-heading__thin--blue">
-			contact
-		</span>
-	</div> -->
 </main>
 <?php get_footer() ;?>
