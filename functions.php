8fb28d288748da75e4657948f92da3b47eaed066
naho1234 <naa01.1105ily@gmail.com>
2023-10-12 16:51:01 +0900

    [add]wp-block-library-css

--- functions.php
+++ functions.php
@@ -103,13 +103,11 @@ function add_description(){
 }
 add_action('wp_head', 'add_description');
 
-// function dequeue_gutenberg_styles() {
-// 	// クラシックエディタを使用している場合
-// 	if ( ! is_use_block_editor_for_post( get_the_ID() ) ) {
-// 			wp_dequeue_style( 'wp-block-library' );
-// 	}
-// }
-// add_action( 'wp_enqueue_scripts', 'dequeue_gutenberg_styles' );
+function dequeue_gutenberg_styles() {
+	wp_dequeue_style( 'wp-block-library' );
+}
+add_action( 'wp_enqueue_scripts', 'dequeue_gutenberg_styles', 100 );
+
 
 // ニュース詳細の「-」を消す
 function custom_wp_title($title, $sep) {
