6b0b95a022f5c9dfed31d3c4044b6e8b54c13391
naho1234 <naa01.1105ily@gmail.com>
2023-10-14 19:20:33 +0900

    [add]テキスト修正

--- front-page.php
+++ front-page.php
@@ -1,229 +1,234 @@
 <?php get_header(); ?>
 <main>
-  <section class="top-fv">
-    <h2 class="color-blue">
-      <span>
-        人と地球に<br />
-        優しい環境作り
-      </span>
-    </h2>
-    <p class="color-white">
-      <span>
-        人と地球に<br />
-        優しい環境作り
-      </span>
-    </p>
-    <p class="color-blue__sub">
-      <span>
-      清掃業界を通じて、温暖化・エコへの取り組みとしての<br>
-      社会貢献活動を続けていきます。
-      </span>
-    </p>
-    <p class="color-white__sub">
-      <span>
-      清掃業界を通じて、温暖化・エコへの取り組みとしての<br>
-      社会貢献活動を続けていきます。
-      </span>
-    </p>
-    <div class="swiper fv-swiper">
-      <div class="swiper-wrapper">
-        <div class="swiper-slide">
-          <div class="swiper-img top-fv__img">
-            <img loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/fv01.jpg" alt="">
-          </div>
-        </div>
-        <div class="swiper-slide">
-          <div class="swiper-img top-fv__img">
-            <img loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/fv02.jpg" alt="">
-          </div>
-        </div>
-        <div class="swiper-slide">
-          <div class="swiper-img top-fv__img">
-            <img loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/fv03.jpg" alt="">
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="mv-scroll">
-      <div class="mv-scroll__title">scroll</div>
-      <div class="mv-bar"></div>
-    </div>
-  </section>
-  <div class="top-fv__banner inner">
-    <a href="<?php echo esc_url(home_url('/lp/')); ?>">
-      <img loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/banner.jpg" alt="">
-    </a>
-  </div>
-  <section class="top-feature">
-    <div class="top-feature__inner">
-      <span class="top-feature__englishtitle">feature</span>
-      <div class="section-heading">
-        <span class="section-heading__subtitle section-heading__subtitle--blue js-fadeUp fadeup fadeup01">feature</span>
-        <h2 class="section-heading__title section-heading__title--black js-fadeUp fadeup fadeup02">エルビーアールのこだわり</h2>
-      </div>
-      <p class="top-feature__summary">当社は、技術力を持つ特殊清掃のプロフェッショナルです。<br>
-        また、環境に対する強い配慮を持ち、エコクリーニングに取り組んでいます。<br class="u-desktop">
-        清掃分野においては確かな専門知識を有し、お客様の要望に合わせたサービスを提供しています。
-      </p>
-      <div class="top-feature__lists">
-        <div class="top-feature__wrap">
-          <img loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/service-feature01.jpg" alt="">
-          <p>技術力の高い特殊清掃</p>
-        </div>
-        <div class="top-feature__wrap">
-          <img loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/service-feature02.jpg" alt="">
-          <p>エコクリーニングへのこだわり</p>
-        </div>
-        <div class="top-feature__wrap">
-          <img loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/service-feature03.jpg" alt="">
-          <p>清掃のスペシャリスト</p>
-        </div>
-      </div>
-      <div class="top-feature__button button">
-        <a href="<?php echo esc_url(home_url('/service/')); ?>">詳しく見る</a>
-      </div>
-    </div>
-  </section>
-  <section class="top-service">
-    <div class="service__inner">
-      <span class="top-service__englishtitle">service</span>
-      <div class="section-heading">
-        <span class="section-heading__subtitle section-heading__subtitle--blue js-fadeUp fadeup fadeup01">service</span>
-        <h2 class="section-heading__title section-heading__title--black js-fadeUp fadeup fadeup02">サービス内容</h2>
-      </div>
-      <p class="service-feature__summary top-service__summary">当社は、多岐にわたる建物タイプに対応し、オフィス、マンション、病院、住宅、商業施設など、<br class="u-desktop">
-        建物の種類やお客様の要望に合わせた高品質なクリーニングサービスを提供しています。<br class="u-desktop">
-        これまで蓄積した幅広い知識と経験に基づき、お客様のニーズに細かく対応し、<br class="u-desktop">
-        建物の清潔さと快適さを確保しています。
-      </p>
-      <div class="top-service__contents">
-        <div class="service__content service__content--right top-service__content--right">
-          <div class="service__text">
-            <h3 class="top-service__heading top-service__heading--01">マンション</h3>
-            <p class="service-feature__text">大切な人と共に過ごす空間を、快適で心地よい場所に変えるお手伝いをさせていただきます。私たちの専門的な清掃で空間の美化に取り組みます。</p>
-            <div class="service__subenglish service__subenglish--right u-desktop">
-              <span class="u-desktop">apartment</span>
-            </div>
-          </div>
-          <div class="service__img">
-            <img loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/lp-service-pc01.jpg" alt="">
-          </div>
-        </div>
-      </div>
-      <div class="top-service__contents top-service__contents--02">
-        <div class="service__content service__content--left">
-          <div class="service__text service__text--white">
-            <h3 class="top-service__heading top-service__heading--02">ビル</h3>
-            <p class="service-feature__text">毎日出社していただく皆様が気持ちよくオフィスにお越しいただけるよう、プロの視点で徹底的な清掃を実施しております。清潔な環境は労働環境の向上に繋がり、生産性や快適さを高めます。私たちはオフィスの清潔さを維持し、快適な作業環境を提供することで、皆様の業務に集中できるようお手伝いさせていただきます。</p>
-            <div class="service__subenglish service__subenglish--left u-desktop">
-              <span class="u-desktop">building</span>
-            </div>
-          </div>
-          <div class="service__img">
-            <img loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/lp-service-pc02.jpg" alt="">
-          </div>
-        </div>
-      </div>
-      <div class="top-service__contents top-service__contents--03">
-        <div class="service__content service__content--right top-service__content--right">
-          <div class="service__text service__text--03">
-            <h3 class="top-service__heading top-service__heading--03">住宅</h3>
-            <p class="service-feature__text">プロの視点から、普段はできない清掃箇所を専門的に清掃作業を行います。普段は見過ごされるか、手が届きにくい場所でも、私たちのプロの清掃スキルと経験を活かし、最高のクオリティでお手伝いさせていただきます。清潔で快適な環境を提供し、お客様の満足度を高めるお手伝いをさせていただきます。</p>
-            <div class="service__subenglish service__subenglish--right u-desktop">
-              <span class="u-desktop">housing</span>
-            </div>
-          </div>
-          <div class="service__img">
-            <img loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/lp-service-pc03.jpg" alt="">
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="top-service__button button">
-      <a href="<?php echo esc_url(home_url('/service/')); ?>#service-contents">詳しく見る</a>
-    </div>
-  </section>
-  	<!--CSSで無限ループ  -->
+	<section class="top-fv">
+		<h2 class="color-blue">
+			<span>
+				人と地球に<br />
+				優しい環境作り
+			</span>
+		</h2>
+		<p class="color-white">
+			<span>
+				人と地球に<br />
+				優しい環境作り
+			</span>
+		</p>
+		<p class="color-blue__sub">
+			<span>
+				清掃業界を通じて、温暖化・エコへの取り組みとしての<br>
+				社会貢献活動を続けていきます。
+			</span>
+		</p>
+		<p class="color-white__sub">
+			<span>
+				清掃業界を通じて、温暖化・エコへの取り組みとしての<br>
+				社会貢献活動を続けていきます。
+			</span>
+		</p>
+		<div class="swiper fv-swiper">
+			<div class="swiper-wrapper">
+				<div class="swiper-slide">
+					<div class="swiper-img top-fv__img">
+						<img loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/fv01.jpg" alt="">
+					</div>
+				</div>
+				<div class="swiper-slide">
+					<div class="swiper-img top-fv__img">
+						<img loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/fv02.jpg" alt="">
+					</div>
+				</div>
+				<div class="swiper-slide">
+					<div class="swiper-img top-fv__img">
+						<img loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/fv03.jpg" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="mv-scroll">
+			<div class="mv-scroll__title">scroll</div>
+			<div class="mv-bar"></div>
+		</div>
+	</section>
+	<div class="top-fv__banner inner">
+		<a href="<?php echo esc_url(home_url('/lp/')); ?>">
+			<img loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/banner.jpg" alt="">
+		</a>
+	</div>
+	<section class="top-feature">
+		<div class="top-feature__inner">
+			<span class="top-feature__englishtitle">feature</span>
+			<div class="section-heading">
+				<span class="section-heading__subtitle section-heading__subtitle--blue js-fadeUp fadeup fadeup01">feature</span>
+				<h2 class="section-heading__title section-heading__title--black js-fadeUp fadeup fadeup02">エルビーアールのこだわり</h2>
+			</div>
+			<p class="top-feature__summary">当社は、高い技術力を持つ特殊清掃のプロフェッショナルです。<br>
+				また、環境に対する強い配慮を持ち、エコクリーニングに取り組んでいます。<br class="u-desktop">
+				清掃分野においては確かな専門知識を有し、お客様の要望に合わせたサービスを提供しています。
+			</p>
+			<div class="top-feature__lists">
+				<div class="top-feature__wrap">
+					<img loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/service-feature01.jpg" alt="">
+					<p>技術力の高い特殊清掃</p>
+				</div>
+				<div class="top-feature__wrap">
+					<img loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/service-feature02.jpg" alt="">
+					<p>エコクリーニングへのこだわり</p>
+				</div>
+				<div class="top-feature__wrap">
+					<img loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/service-feature03.jpg" alt="">
+					<p>清掃のスペシャリスト</p>
+				</div>
+			</div>
+			<div class="top-feature__button button">
+				<a href="<?php echo esc_url(home_url('/service/')); ?>">詳しく見る</a>
+			</div>
+		</div>
+	</section>
+	<section class="top-service">
+		<div class="service__inner">
+			<span class="top-service__englishtitle">service</span>
+			<div class="section-heading">
+				<span class="section-heading__subtitle section-heading__subtitle--blue js-fadeUp fadeup fadeup01">service</span>
+				<h2 class="section-heading__title section-heading__title--black js-fadeUp fadeup fadeup02">サービス内容</h2>
+			</div>
+			<p class="service-feature__summary top-service__summary">当社は、多岐にわたる建物タイプに対応し、オフィス、マンション、病院、住宅、商業施設など、<br
+					class="u-desktop">
+				建物の種類やお客様の要望に合わせた高品質なクリーニングサービスを提供しています。<br class="u-desktop">
+				これまで蓄積した幅広い知識と経験に基づき、お客様のニーズに細かく対応し、<br class="u-desktop">
+				建物の清潔さと快適さを確保しています。
+			</p>
+			<div class="top-service__contents">
+				<div class="service__content service__content--right top-service__content--right">
+					<div class="service__text">
+						<h3 class="top-service__heading top-service__heading--01">マンション</h3>
+						<p class="service-feature__text">大切な人と共に過ごす空間を、快適で心地よい場所に変えるお手伝いをさせていただきます。私たちの専門的な清掃で空間の美化に取り組みます。</p>
+						<div class="service__subenglish service__subenglish--right u-desktop">
+							<span class="u-desktop">apartment</span>
+						</div>
+					</div>
+					<div class="service__img">
+						<img loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/lp-service-pc01.jpg" alt="">
+					</div>
+				</div>
+			</div>
+			<div class="top-service__contents top-service__contents--02">
+				<div class="service__content service__content--left">
+					<div class="service__text service__text--white">
+						<h3 class="top-service__heading top-service__heading--02">ビル</h3>
+						<p class="service-feature__text">
+							毎日出社していただく皆様が気持ちよくオフィスにお越しいただけるよう、プロの視点で徹底的な清掃を実施しております。清潔な環境は労働環境の向上に繋がり、生産性や快適さを高めます。私たちはオフィスの清潔さを維持し、快適な作業環境を提供することで、皆様の業務に集中できるようお手伝いさせていただきます。
+						</p>
+						<div class="service__subenglish service__subenglish--left u-desktop">
+							<span class="u-desktop">building</span>
+						</div>
+					</div>
+					<div class="service__img">
+						<img loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/lp-service-pc02.jpg" alt="">
+					</div>
+				</div>
+			</div>
+			<div class="top-service__contents top-service__contents--03">
+				<div class="service__content service__content--right top-service__content--right">
+					<div class="service__text service__text--03">
+						<h3 class="top-service__heading top-service__heading--03">住宅</h3>
+						<p class="service-feature__text">
+							プロの視点から、普段はできない清掃箇所を専門的に清掃作業を行います。普段は見過ごされるか、手が届きにくい場所でも、私たちのプロの清掃スキルと経験を活かし、最高のクオリティでお手伝いさせていただきます。清潔で快適な環境を提供し、お客様の満足度を高めるお手伝いをさせていただきます。
+						</p>
+						<div class="service__subenglish service__subenglish--right u-desktop">
+							<span class="u-desktop">housing</span>
+						</div>
+					</div>
+					<div class="service__img">
+						<img loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/lp-service-pc03.jpg" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="top-service__button button">
+			<a href="<?php echo esc_url(home_url('/service/')); ?>#service-contents">詳しく見る</a>
+		</div>
+	</section>
+	<!--CSSで無限ループ  -->
 	<div class="slide top-slide">
 		<div class="slide__wrap">
 			<ul class="slide__list slide__list--left top-slide__list">
-				<li class="slide__item top-slide__item"><img
-						src="<?php echo get_template_directory_uri() ?>/images/slide01.png" alt="">
+				<li class="slide__item top-slide__item"><img src="<?php echo get_template_directory_uri() ?>/images/slide01.png"
+						alt="">
 				</li>
-				<li class="slide__item top-slide__item"><img
-						src="<?php echo get_template_directory_uri() ?>/images/slide02.png" alt="">
+				<li class="slide__item top-slide__item"><img src="<?php echo get_template_directory_uri() ?>/images/slide02.png"
+						alt="">
 				</li>
-				<li class="slide__item top-slide__item"><img
-						src="<?php echo get_template_directory_uri() ?>/images/slide03.png" alt="">
+				<li class="slide__item top-slide__item"><img src="<?php echo get_template_directory_uri() ?>/images/slide03.png"
+						alt="">
 				</li>
-				<li class="slide__item top-slide__item"><img
-						src="<?php echo get_template_directory_uri() ?>/images/slide04.png" alt="">
+				<li class="slide__item top-slide__item"><img src="<?php echo get_template_directory_uri() ?>/images/slide04.png"
+						alt="">
 				</li>
 			</ul>
 			<ul class="slide__list slide__list--left">
-				<li class="slide__item top-slide__item"><img
-						src="<?php echo get_template_directory_uri() ?>/images/slide01.png" alt="">
+				<li class="slide__item top-slide__item"><img src="<?php echo get_template_directory_uri() ?>/images/slide01.png"
+						alt="">
 				</li>
-				<li class="slide__item top-slide__item"><img
-						src="<?php echo get_template_directory_uri() ?>/images/slide02.png" alt="">
+				<li class="slide__item top-slide__item"><img src="<?php echo get_template_directory_uri() ?>/images/slide02.png"
+						alt="">
 				</li>
-				<li class="slide__item top-slide__item"><img
-						src="<?php echo get_template_directory_uri() ?>/images/slide03.png" alt="">
+				<li class="slide__item top-slide__item"><img src="<?php echo get_template_directory_uri() ?>/images/slide03.png"
+						alt="">
 				</li>
-				<li class="slide__item top-slide__item"><img
-						src="<?php echo get_template_directory_uri() ?>/images/slide04.png" alt="">
+				<li class="slide__item top-slide__item"><img src="<?php echo get_template_directory_uri() ?>/images/slide04.png"
+						alt="">
 				</li>
 			</ul>
-      <ul class="slide__list slide__list--left">
-				<li class="slide__item top-slide__item"><img
-						src="<?php echo get_template_directory_uri() ?>/images/slide01.png" alt="">
+			<ul class="slide__list slide__list--left">
+				<li class="slide__item top-slide__item"><img src="<?php echo get_template_directory_uri() ?>/images/slide01.png"
+						alt="">
 				</li>
-				<li class="slide__item top-slide__item"><img
-						src="<?php echo get_template_directory_uri() ?>/images/slide02.png" alt="">
+				<li class="slide__item top-slide__item"><img src="<?php echo get_template_directory_uri() ?>/images/slide02.png"
+						alt="">
 				</li>
-				<li class="slide__item top-slide__item"><img
-						src="<?php echo get_template_directory_uri() ?>/images/slide03.png" alt="">
+				<li class="slide__item top-slide__item"><img src="<?php echo get_template_directory_uri() ?>/images/slide03.png"
+						alt="">
 				</li>
-				<li class="slide__item top-slide__item"><img
-						src="<?php echo get_template_directory_uri() ?>/images/slide04.png" alt="">
+				<li class="slide__item top-slide__item"><img src="<?php echo get_template_directory_uri() ?>/images/slide04.png"
+						alt="">
 				</li>
 			</ul>
 		</div>
 	</div>
-  <?php
+	<?php
   $args = [
     'post_type' => 'post',
     'posts_per_page' => 4, // 表示件数
   ];
   $the_query = new WP_Query($args);
   ?>
-  <section class="top-news">
-    <div class="section-heading">
-      <span class="section-heading__subtitle section-heading__subtitle--blue js-fadeUp fadeup fadeup01">news</span>
-      <h2 class="section-heading__title section-heading__title--black js-fadeUp fadeup fadeup02">お知らせ</h2>
-    </div>
-    <div class="top-news__wrap">
-      <div class="top-news__contents">
-        <?php if ($the_query->have_posts()) : ?>
-          <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
-            <div class="top-news__content news-item">
-              <time class="news-item__date" datetime="<?php the_time('c'); ?>"><?php the_time('Y/m/d'); ?></time>
-              <p class="news-item__title">
-                <a href="<?php the_permalink() ?>"><?php the_title() ?></a>
-              </p>
-            </div>
-        <?php endwhile;
+	<section class="top-news">
+		<div class="section-heading">
+			<span class="section-heading__subtitle section-heading__subtitle--blue js-fadeUp fadeup fadeup01">news</span>
+			<h2 class="section-heading__title section-heading__title--black js-fadeUp fadeup fadeup02">お知らせ</h2>
+		</div>
+		<div class="top-news__wrap">
+			<div class="top-news__contents">
+				<?php if ($the_query->have_posts()) : ?>
+				<?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+				<div class="top-news__content news-item">
+					<time class="news-item__date" datetime="<?php the_time('c'); ?>"><?php the_time('Y/m/d'); ?></time>
+					<p class="news-item__title">
+						<a href="<?php the_permalink() ?>"><?php the_title() ?></a>
+					</p>
+				</div>
+				<?php endwhile;
         endif; ?>
-      </div>
-      <div class="top-news__button button">
-        <a href="<?php echo esc_url(home_url('/news/')); ?>">詳しく見る</a>
-      </div>
-    </div>
-    <span class="top-news__englishtitle">news</span>
-  </section>
-  <a class="pagetop" href="#">
-    <div class="pagetop__arrow"></div>
-  </a>
+			</div>
+			<div class="top-news__button button">
+				<a href="<?php echo esc_url(home_url('/news/')); ?>">詳しく見る</a>
+			</div>
+		</div>
+		<span class="top-news__englishtitle">news</span>
+	</section>
+	<a class="pagetop" href="#">
+		<div class="pagetop__arrow"></div>
+	</a>
 </main>
 <?php get_template_part('template/contact-footer'); ?>
 
